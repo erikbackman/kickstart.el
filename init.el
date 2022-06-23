c2c90aa310ccf3049e535e76d9b7fb1278cc13fc
erikbackman <erikbackman@users.noreply.github.com>
2022-06-23 07:36:57 +0200

    comments

--- init.el
+++ init.el
@@ -100,7 +100,7 @@ or the current line if there is no active region."
   ;; See: https://www.gnu.org/software/emacs/manual/html_node/elisp/Dotted-Pair-Notation.html
   ;; for information on "dotted pair notation" i.e (foo . bar)
   ;; or press 'C-h i' (to access the info-system) then use 'm' to navigate to:
-  ;; File: Elisp > Dotted Pair Notation
+  ;; Elisp > Dotted Pair Notation
   (:map global-map
 	("C-8" . backward-list)
 	("C-9" . forward-list)
